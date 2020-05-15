2228290e558b31f5f5dd9203337521553c7b4ba2
nucaiurie <43534339+nucaiurie@users.noreply.github.com>
2020-05-15 19:25:47 +0300

    Add sidebar, post menu, logo

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.filter(status='p').order_by('created_at')
+    return render(request, 'posts/post_list.html', {'posts': posts})
